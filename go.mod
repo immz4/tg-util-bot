7948ddb7c722401f2ed97dbeb1b6c16c641173f0
Alexander Kulichkov <hetyh2004@gmail.com>
2025-01-03 02:07:22 +0100

    Added resend functionality

--- go.mod
+++ go.mod
@@ -2,6 +2,27 @@ module github.com/hetyh/tg-util-bot
 
 go 1.23.4
 
-require gopkg.in/telebot.v4 v4.0.0-beta.4
+require (
+	github.com/go-playground/validator/v10 v10.23.0
+	github.com/knadh/koanf/parsers/toml/v2 v2.1.0
+	github.com/knadh/koanf/providers/rawbytes v0.1.0
+	github.com/knadh/koanf/v2 v2.1.2
+	gopkg.in/telebot.v4 v4.0.0-beta.4
+)
 
-require github.com/stretchr/testify v1.10.0 // indirect
+require (
+	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,55 +3,128 @@ package main
 import (
 	"fmt"
 	"log"
-	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/knadh/koanf/parsers/toml/v2"
+	"github.com/knadh/koanf/providers/rawbytes"
+	"github.com/knadh/koanf/v2"
 	tele "gopkg.in/telebot.v4"
 )
 
-func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+type ResendTarget string
 
+func (id ResendTarget) Recipient() string {
+	return string(id)
+}
+
+type Source struct {
+	Type string `koanf:"type" validate:"required,oneof='channel' 'group'"`
+	Id   int64 `koanf:"id" validate:"required"`
+}
+
+type Config struct {
+	Token  string `koanf:"token" validate:"required,len=46"`
+	Resend struct {
+		Enabled bool   `koanf:"enabled"`
+		From    Source `koanf:"from" validate:"required_if=Enabled true"`
+		To      Source `koanf:"to" validate:"required_if=Enabled true"`
+	} `koanf:"resend"`
+	Feedback struct {
+		Enabled bool   `koanf:"enabled"`
+		ChatId  string `koanf:"chatId" validate:"required_if=Enabled true"`
+	} `koanf:"feedback"`
+}
+
+var (
+	k        = koanf.New(".")
+	validate = validator.New(validator.WithRequiredStructEnabled())
+)
+
+func loadConfig(config string) Config {
+	if err := k.Load(rawbytes.Provider([]byte(config)), toml.Parser()); err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+
+	var configData Config
+
+	if err := k.Unmarshal("", &configData); err != nil {
+		panic(err)
+	}
+
+	if err := validate.Struct(configData); err != nil {
+		panic(err)
+	}
+
+	return configData
+}
+
+func main() {
+	config := loadConfig(os.Getenv("APP_CONFIG"))
+
+	// Health check endpoint
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "OK")
 	})
 
+	// Telegram bot setup
 	pref := tele.Settings{
-		Token:  os.Getenv("TOKEN"),
+		Token:  config.Token,
 		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
 	}
 
 	b, err := tele.NewBot(pref)
 	if err != nil {
-		log.Fatal(err)
-		return
+		panic(err)
 	}
 
-	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
+	b.Handle("/id", func (c tele.Context) error {
+		id := strconv.Itoa(int(c.Chat().ID))
+		log.Println("Got /id", id)
+		return c.Send(strconv.Itoa(int(c.Chat().ID)))
 	})
 
+	if config.Resend.Enabled {
+		log.Println("Enabling resend functionality")
+
+		target := ResendTarget(strconv.Itoa(int(config.Resend.To.Id)))
+
+		b.Handle("/forward", func(c tele.Context) error {
+			log.Println("Got /forward", c.Chat().ID)
+
+			if c.Message().IsReply() && c.Chat().ID == config.Resend.From.Id {
+				_, err := b.Forward(target, c.Message().ReplyTo)
+				return err
+			}
+			return nil
+		})
+	}
+
+	b.SetCommands()
+
+	// Starting the app
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 
 	go func() {
 		defer wg.Done()
 
-		logger.Info("Starting HTTP server")
+		log.Println("Starting HTTP server")
 		log.Fatal(http.ListenAndServe(":3000", nil))
 	}()
 
 	go func() {
 		defer wg.Done()
 
-		logger.Info("Starting Telegram bot")
+		log.Println("Starting Telegram bot")
 		b.Start()
 	}()
 
 	wg.Wait()
 
-	fmt.Println("a vse")
+	fmt.Println("a gde)")
 }
